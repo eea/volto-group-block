d078df74648f0673f1cf5d7c537ff0b2558654c2
Alin Voinea <contact@avoinea.com>
2020-09-22 23:37:40 +0300

    Avoid removing all blocks within meta-block

--- src/components/manage/Blocks/Meta/Edit.jsx
+++ src/components/manage/Blocks/Meta/Edit.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
 import { useState } from 'react';
-import { v4 as uuid } from 'uuid';
+import { isEmpty } from 'lodash';
 import { BlocksForm } from '@eeacms/volto-blocks-form/components';
+import { emptyBlocksForm } from '@eeacms/volto-blocks-form/helpers';
 import './editor.less';
-import { settings } from '~/config';
-
-const emptyBlocksForm = () => {
-  const id = uuid();
-  return {
-    blocks: {
-      [id]: {
-        '@type': settings.defaultBlockType,
-        required: true,
-      },
-    },
-    blocks_layout: { items: [id] },
-  };
-};
 
 export const MetaBlockEdit = (props) => {
   const { block, data, onChangeBlock, pathname, selected, manage } = props;
 
-  const properties = data.data || emptyBlocksForm();
+  const properties = isEmpty(data?.data?.blocks)
+    ? emptyBlocksForm()
+    : data.data;
   const [selectedBlock, setSelectedBlock] = useState();
   const blockState = {};
 
